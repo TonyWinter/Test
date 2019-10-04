3d527e2cfe42d1aa51bc4624b80903fedcc353c1
Anton Tschistjakov <opugamoschke@hotmail.com>
2019-10-04 17:41:56 +0300

    Version 2

--- ModernIt/ModernIt/Program.vb
+++ ModernIt/ModernIt/Program.vb
@@ -3,6 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
-
+        Console.WriteLine("The app demostrate a wok with Git")
+        Console.WriteLine("This sentence will be changed")
     End Sub
 End Module
